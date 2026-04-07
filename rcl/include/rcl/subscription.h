7695aa4f00319cb95407f80fcce2a89901660062
Rushhaank Sahay <155910992+rushsahay@users.noreply.github.com>
2026-04-07 04:16:20 -0500

    Improved documentation of rcl_XYZ_set_on_new_XYZ_callback (#1289)
    * Improved documentation of rcl_XYZ_set_on_new_XYZ_callback
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>
    
    * Added description to rcl_client_set_on_new_response_callback
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>
    
    * Specify consequences of blocking
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>
    
    * Refix the message after fixing signing off
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>
    
    * Added possible consequences and why it should not be blocking
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>
    
    * Reformatted using uncrustify
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>
    
    * Fixed formatting
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>
    
    ---------
    
    Signed-off-by: Rushsahay <rushhaank.sahay@gmail.com>

--- rcl/include/rcl/subscription.h
+++ rcl/include/rcl/subscription.h
@@ -910,6 +910,17 @@ rcl_subscription_can_loan_messages(const rcl_subscription_t * subscription);
  * \sa rmw_subscription_set_on_new_message_callback for details about this
  * function.
  *
+ * Since this callback is called from the middleware, you should
+ * aim to make it fast and not blocking. This callback
+ * is intended to implement an event driven executor and
+ * not process data directly.
+ *
+ * Doing work in this callback can cause delays,
+ * deadlocks, or latency due to cross thread waiting
+ * as this process runs on middleware managed threads
+ * and is meant only to notify the executor that new data
+ * is available in the middleware queue.
+ *
  * <hr>
  * Attribute          | Adherence
  * ------------------ | -------------
